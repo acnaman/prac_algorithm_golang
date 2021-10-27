6cde572e728d86a7e5f794c3479eab9ae8f8c823
acnaman <acnadin01@gmail.com>
2021-10-27 23:43:37 +0900

    バブルソートの実装

--- bubbleSort.go
+++ bubbleSort.go
@@ -1,5 +1,21 @@
 package main
 
-func BubbleSort(l []int) []int {
-	return l
+/**
+ * 引数のスライスをバブルソートでソートする。
+ * 引数のスライスを直接並び替えする副作用のあるメソッドであることに注意。
+ */
+func BubbleSort(target []int) []int {
+	length := len(target)
+
+	for round := 0; round < length; round++ {
+		for i := length - 1; i > round; i-- {
+			left := target[i-1]
+
+			if left > target[i] {
+				target[i-1] = target[i]
+				target[i] = left
+			}
+		}
+	}
+	return target
 }
