126ae33c6c302a540f087dd2db9bb09aed528c00
acnaman <acnadin01@gmail.com>
2021-10-27 23:18:13 +0900

    テストの追加

---
+++ bubbleSort_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestBubbleSort(t *testing.T) {
+	t.Run("[1,2,3]をバブルソートすると[1,2,3]となる", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		expected := []int{1, 2, 3}
+		actual := BubbleSort(numbers)
+		if !equals(actual, expected) {
+			t.Errorf("got %v want %v given, %v", numbers, expected, numbers)
+		}
+	})
+}
+
+/**
+ * intのスライスの完全一致を判定する
+ */
+func equals(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
