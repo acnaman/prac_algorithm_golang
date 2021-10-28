2edad6cd9c25eabffa4636bc507cd09cefcd7482
acnaman <acnadin01@gmail.com>
2021-10-28 19:30:11 +0900

    選択ソートの実装

--- selection_sort.go
+++ selection_sort.go
@@ -5,5 +5,22 @@ package main
  * 引数のスライスを直接並び替えする副作用のあるメソッドであることに注意。
  */
 func SelectionSort(target []int) []int {
+	length := len(target)
+
+	for round := 0; round < length; round++ {
+		first := target[round]
+		min := first
+		minIndex := round
+
+		for i := round + 1; i < length; i++ {
+			if min > target[i] {
+				min = target[i]
+				minIndex = i
+			}
+		}
+		target[round] = min
+		target[minIndex] = first
+	}
+
 	return target
 }
