453012a7aa93f5f8bd34517bff49c040b5f2d4fa
acnaman <acnadin01@gmail.com>
2021-10-30 12:39:29 +0900

    挿入ソートの実装を追加

--- insertion_sort.go
+++ insertion_sort.go
@@ -5,5 +5,21 @@ package main
  * 引数のスライスを直接並び替えする副作用のあるメソッドであることに注意。
  */
 func InsertionSort(target []int) []int {
+	length := len(target)
+
+	for round := 0; round < length; round++ {
+		selectedObject := target[round]
+
+		for i := round; i > 0; i-- {
+			comparisonObject := target[i-1]
+
+			if comparisonObject > selectedObject {
+				target[i-1] = selectedObject
+				target[i] = comparisonObject
+			} else {
+				break
+			}
+		}
+	}
 	return target
 }
