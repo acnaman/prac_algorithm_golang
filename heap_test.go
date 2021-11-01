be5881c4ac372ade1e0c31d937e37ef28cbff938
acnaman <acnadin01@gmail.com>
2021-11-01 23:07:15 +0900

    test: ヒープのノード追加メソッドのテストを複数パターン追加

--- heap_test.go
+++ heap_test.go
@@ -16,4 +16,40 @@ func TestHeapInsert(t *testing.T) {
 			t.Errorf("got %v want %v given, %v", actual, expected, heap)
 		}
 	})
+
+	t.Run("[1,3,5]に2を挿入すると[1,2,5,3]となる", func(t *testing.T) {
+		heap := Heap{[]int{1, 3, 5}}
+		expected := []int{1, 2, 5, 3}
+
+		heap.Insert(2)
+		actual := heap.nodes
+
+		if !Equals(actual, expected) {
+			t.Errorf("got %v want %v given, %v", actual, expected, heap)
+		}
+	})
+
+	t.Run("[1,3,6,4,8,7]に5を挿入すると[1,3,5,4,8,7,6]となる", func(t *testing.T) {
+		heap := Heap{[]int{1, 3, 6, 4, 8, 7}}
+		expected := []int{1, 3, 5, 4, 8, 7, 6}
+
+		heap.Insert(5)
+		actual := heap.nodes
+
+		if !Equals(actual, expected) {
+			t.Errorf("got %v want %v given, %v", actual, expected, heap)
+		}
+	})
+
+	t.Run("[2,3,6,4,8,7]に1を挿入すると[1,3,2,4,8,7,6]となる", func(t *testing.T) {
+		heap := Heap{[]int{2, 3, 6, 4, 8, 7}}
+		expected := []int{1, 3, 2, 4, 8, 7, 6}
+
+		heap.Insert(1)
+		actual := heap.nodes
+
+		if !Equals(actual, expected) {
+			t.Errorf("got %v want %v given, %v", actual, expected, heap)
+		}
+	})
 }
