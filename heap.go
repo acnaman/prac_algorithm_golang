25418c062c4d87edbf037e86d476e1532ff1101e
acnaman <acnadin01@gmail.com>
2021-11-01 21:59:57 +0900

    feat: ヒープの親ノード・子ノードを取得する関数を作成

--- heap.go
+++ heap.go
@@ -3,3 +3,18 @@ package main
 type Heap struct {
 	nodes []int
 }
+
+// 指定されたインデックスのノードの親ノードのインデックスを取得します。
+func parentNodeIndex(i int) int {
+	return (i - 1) / 2
+}
+
+// 指定されたインデックスのノードの子ノードのうち、左のノードのインデックスを取得します。
+func leftChildrenNodeIndex(i int) int {
+	return 2*i + 1
+}
+
+// 指定されたインデックスのノードの子ノードのうち、右のノードのインデックスを取得します。
+func rightChildrenNodeIndex(i int) int {
+	return 2*i + 2
+}
