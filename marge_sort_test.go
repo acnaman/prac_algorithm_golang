55cbf031135452944e348e82374d3727b6b995be
acnaman <acnadin01@gmail.com>
2021-11-11 00:00:03 +0900

    feat: マージソートのテストを追加

---
+++ marge_sort_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestMargeSort(t *testing.T) {
+	t.Run("[1,2,3]をマージソートすると[1,2,3]となる", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		expected := []int{1, 2, 3}
+		actual := MargeSort(numbers)
+		if !Equals(actual, expected) {
+			t.Errorf("got %v want %v given, %v", actual, expected, numbers)
+		}
+	})
+
+	t.Run("[2,1,3]をマージソートすると[1,2,3]となる", func(t *testing.T) {
+		numbers := []int{2, 1, 3}
+		expected := []int{1, 2, 3}
+		actual := MargeSort(numbers)
+		if !Equals(actual, expected) {
+			t.Errorf("got %v want %v given, %v", actual, expected, numbers)
+		}
+	})
+
+	t.Run("[5,9,3,1,2,8,4,6,7]をマージソートすると[1,2,3,4,5,6,7,8,9]となる", func(t *testing.T) {
+		numbers := []int{5, 9, 3, 1, 2, 8, 4, 6, 7}
+		expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+		actual := MargeSort(numbers)
+		if !Equals(actual, expected) {
+			t.Errorf("got %v want %v given, %v", actual, expected, numbers)
+		}
+	})
+
+}
